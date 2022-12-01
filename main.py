2b92537d387d949609c41565ad19a57422924e84
robertdeg <robertonl@gmail.com>
2022-12-01 06:23:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
-    cals = list(map(lambda s: sum(map(int, s.split('\n'))), open("input/day1.txt").read().split('\n\n')))
-    print(f"day 1 - part 1: {max(cals)}")
-    print(f"day 1 - part 2: {sum(sorted(cals, reverse=True)[:3])}")
+    cals = sorted(map(lambda s: sum(map(int, s.split('\n'))), open("input/day1.txt").read().split('\n\n')), reverse=True)[:3]
+    print(f"day 1 - part 1: {cals[0]}, part 2: {sum(cals)}")
 
