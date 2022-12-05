bd340ca4fea1fb6c4829e684bb8741e1ec6eb66c
robertdeg <robertonl@gmail.com>
2022-12-05 15:18:16 +0100

    day 5 done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
+import collections
+from itertools import zip_longest, starmap
 import operator as ops
-from functools import reduce
+from functools import reduce, partial
 from collections import Counter
 import numpy as np
 import re
@@ -33,16 +34,45 @@ def day4(filename: str):
     lines = (l.strip() for l in open(filename).read().split())
     ranges = (re.match(r"(\d+)-(\d+),(\d+)-(\d+)", line).groups() for line in lines)
     ranges = [(int(a), int(b), int(c), int(d)) for a, b, c, d in ranges]
-    contains = lambda a, b, c, d : a <= c <= d <= b or c <= a <= b <= d
-    overlaps = lambda a, b, c, d : not(b < c or d < a)
-    part1 = sum(1 for a, b, x, y in ranges if contains(a, b, x, y))
-    part2 = sum(1 for a, b, x, y in ranges if overlaps(a, b, x, y))
+    part1 = sum(a <= c <= d <= b or c <= a <= b <= d for a, b, c, d in ranges)
+    part2 = sum(not(b < c or d < a) for a, b, c, d in ranges)
     print(f"day 4 - part 1: {part1}, part 2: {part2}")
+
+def dropwhile(predicate, iterable):
+    # dropwhile(lambda x: x<5, [1,4,6,4,1]) --> 6 4 1
+    iterable = iter(iterable)
+    for x in iterable:
+        if not predicate(x):
+            yield x
+            break
+    for x in iterable:
+        yield x
+def day5(filename: str):
+    def move(source: list, dest: list, count: int, reverse=True) -> None:
+        stack = source[-count:]
+        dest.extend(reversed(stack) if reverse else stack)
+        del source[-count:]
+
+    stack_data, moves_list = map(str.splitlines, open(filename).read().split('\n\n'))
+    blocks = [[line[i+1:i+2] for i in range(0, len(line), 4)] for line in stack_data[:-1]]
+    stacks = [list(''.join(reversed(stack)).strip()) for stack in zip_longest(*blocks, fillvalue='')]
+    moves = (re.match(r'move (\d+) from (\d+) to (\d+)', move).groups() for move in moves_list)
+    moves = [(int(n), int(src), int(dst)) for n, src, dst in moves]
+
+    collections.deque(starmap(lambda n, src, dst : move(stacks[src-1], stacks[dst-1], n), moves), maxlen=0)
+    part1 = ''.join([stack[-1] for stack in stacks])
+
+    stacks = [list(''.join(reversed(stack)).strip()) for stack in zip_longest(*blocks, fillvalue='')]
+    collections.deque(starmap(lambda n, src, dst : move(stacks[src-1], stacks[dst-1], n, reverse=False), moves), maxlen=0)
+    part2 = ''.join([stack[-1] for stack in stacks])
+
+    print(f"day 5 - part 1: {part1}, part 2: {part2}")
 
 if __name__ == '__main__':
     day1("input/day1.txt")
     day2("input/day2.txt")
     day3("input/day3.txt")
     day4("input/day4.txt")
+    day5("input/day5.txt")
 
 
